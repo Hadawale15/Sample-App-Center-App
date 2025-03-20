b421913a4cd71e2865e6103744f593121941e8a6
Hadawale15 <shubhamhadawale15@gmail.com>
2025-03-20 13:02:10 +0530

    First Commit

---
+++ app/src/main/java/com/example/sampleappcenterapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sampleappcenterapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AppCenter.start(getApplication(), "4a8d0a9f-e80e-4174-9d77-a32c67f81145",
+                Analytics.class, Crashes.class);
+    }
+
+}
