451ebf9ad4c51ae20cf96d09e8c4a7d1030cb383
Hadawale15 <shubhamhadawale15@gmail.com>
2025-03-21 12:57:57 +0530

    added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.sampleappcenterapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.sampleappcenterapp"
